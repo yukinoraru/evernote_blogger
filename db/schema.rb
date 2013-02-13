f65bc5f485a79416073514ca7a3b8ca858993f6a
macchima23 <macchima23@gmail.com>
2013-02-13 21:37:18 +0900

    記事の内容を文字だけ表示するようにした

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130213121245) do
+ActiveRecord::Schema.define(:version => 20130213123242) do
 
   create_table "entries", :force => true do |t|
     t.string   "title"
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.datetime "original_created_at"
+    t.text     "content"
   end
 
 end
--- lib/tasks/en_spider.rake
+++ lib/tasks/en_spider.rake
@@ -12,10 +12,10 @@ namespace :en_spider do
     note_list.notes.each do |note|
       #resources    = es.get_note_resources(note, authToken)
       #tags         = es.get_note_tags(note, authToken)
-      #xml_content  = es.get_note_xml(note, authToken)
       entry = Entry.new
       entry.title = note.title
       entry.original_created_at = Time.at(note.created.to_i / 1000)
+      entry.content = es.get_note_xml(note, authToken)
       entry.save
     end
   end
